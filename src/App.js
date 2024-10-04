4c815547a4eda83d2728ce272898c9f66f4ac9c1
Shashank-4502 <106006842+Shashank-4502@users.noreply.github.com>
2024-10-04 21:59:55 +0530

    Revert "implemented a simple accordian using useState , with multi selection …"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import "./App.css";
-import Accordian from "./components/accordian";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* Accordian component */}
-      <Accordian />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
